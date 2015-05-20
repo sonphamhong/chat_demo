2c88b38921f9079de051e2d48c68ddd2d565385b
Sonph <son.ph@anlab.biz>
2015-05-20 10:54:00 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrivatePubChat::Application.config.secret_token = 'e6952f83380c147bb7f9d6f0988b0fd21f5f2759211f829e84f74e1265e0cc30d1c7a852a40cff142c250f71ed6077987340782f48660e31e395d340af1ea6de'
